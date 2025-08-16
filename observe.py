da3a1a8047ea33202a929787f7eef3285d1fce02
Edchel Stephen Nini <ed@pruuv.com>
2025-08-16 15:39:59 +0800

    chore: observe being called.

---
+++ observe.py
@@ -0,0 +1,17 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleSpanProcessor,
+)
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+
+span_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+
+with tracer.start_as_current_span("observe-span"):
+    print("Hello Observability!")
+    print("Dodot is here!")
